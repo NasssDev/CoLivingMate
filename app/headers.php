5229d2dd4f22145ea6f840517a8d178d9f26d5d8
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-08-29 23:31:28 +0200

    headers

--- app/headers.php
+++ app/headers.php
@@ -1,6 +1,6 @@
 <?php
 
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: http://localhost:5173,https://coliving-mate.vercel.app');
+header('Access-Control-Allow-Origin: https://coliving-mate.vercel.app');
 header('Access-Control-Allow-Headers: content-type, authorization');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
