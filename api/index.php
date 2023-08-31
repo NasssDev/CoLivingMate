f0c9843de254ee8cbc22fcce4a1c5690367fded4
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-09-01 01:29:11 +0200

    prod config

---
+++ api/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "nass";
+require __DIR__ . '/../app/index.php';
--- app/headers.php
+++ app/headers.php
@@ -1,6 +1,22 @@
 <?php
 
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: https://coliving-mate.vercel.app');
-header('Access-Control-Allow-Headers: content-type, authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+$allowedOrigins = array(
+    'https://coliving-mate.vercel.app',
+    'http://localhost:5173',
+    'https://coliving-mate-nasssdev.vercel.app/'
+);
+
+$origin = $_SERVER['HTTP_ORIGIN'];
+
+if (in_array($origin, $allowedOrigins)) {
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Origin: ' . $origin);
+    header('Access-Control-Allow-Headers: content-type, authorization');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+}
+
+
+
+
+
+
