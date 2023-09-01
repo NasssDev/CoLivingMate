4db0118656f5265e3c77b581b86209f620aa39cf
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-09-01 23:07:21 +0200

    config

--- public/src/Factories/PDOFactory.php
+++ public/src/Factories/PDOFactory.php
@@ -11,7 +11,7 @@ class PDOFactory implements Database
     private string $userName;
     private string $password;
 
-    public function __construct(string $host = "database", string $dbName = "data", string $userName = "root", string $password = "password")
+    public function __construct(string $host = "b3krsqonxeyllcoo0dpz-mysql.services.clever-cloud.com", string $dbName = "b3krsqonxeyllcoo0dpz", string $userName = "upuaunwfpsgkjf7r", string $password = "tCAsxVaKGXkOnJeDURUr")
     {
         $this->host = $host;
         $this->dbName = $dbName;
